3b4fd387fb46abf5c0aded13bc9dd4a4d1524827
Ahmad Safarjalani <ahmadsaf2007@gmail.com>
2026-05-20 10:33:11 +0300

    Add NodeTable component with actions

--- src/features/nodes/NodeTable.tsx
+++ src/features/nodes/NodeTable.tsx
@@ -1,3 +1,48 @@
+import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, IconButton, Paper } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import type { Node } from '../../types/Node';
+
+interface Props {
+  nodes: Node[];
+  onEdit: (node: Node) => void;
+  onDelete: (node: Node) => void;
+}
+
+export default function NodeTable({ nodes, onEdit, onDelete }: Props) {
+  return (
+    <TableContainer component={Paper}>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Type</TableCell>
+            <TableCell align="right">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {nodes.map((node) => (
+            <TableRow key={node.id} hover>
+              <TableCell>{node.id}</TableCell>
+              <TableCell>{node.name}</TableCell>
+              <TableCell>{node.type}</TableCell>
+              <TableCell align="right">
+                <IconButton size="small" onClick={() => onEdit(node)} title="Edit">
+                  <EditIcon fontSize="small" />
+                </IconButton>
+                <IconButton size="small" onClick={() => onDelete(node)} title="Delete">
+                  <DeleteIcon fontSize="small" />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
 export const NodeTable = () => {
   return <div>NodeTable</div>
 }
